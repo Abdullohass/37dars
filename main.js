ef1df5d79b9b68017905c4bf4437b5d2bc97d15f
Abdullohass <abdulloh.s.gk@gmail.com>
2024-08-17 15:12:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn")
+const filter = document.getElementById("filter")
+const modal = document.getElementById("modal")
+
+btn.addEventListener("click", () => {
+    filter.style = "filter: blur(18px);";
+    modal.style = "display:block";
+})
+window.addEventListener("click", (e) => {
+    if (e.target.id == "btn") {
+        filter.style = "filter: blur(18px);";
+        modal.style = "display:block";
+    } else {
+        filter.style = "filter: blur(0);";
+        modal.style = "display:none";
+    }
+
+})
